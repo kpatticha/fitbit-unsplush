d8b6d4614c504a442265d1109cbe7feaafe64f27
Kate Patticha <aikaterini.patticha@elastic.co>
2024-01-12 17:06:35 +0200

    Display today's activity steps

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import { me } from "appbit";
 import { HeartRateSensor } from "heart-rate";
+import { today } from "user-activity";
 import { preferences } from "user-settings";
 import clock from "clock";
 import document from "document";
@@ -12,6 +13,8 @@ clock.granularity = "minutes";
 const hour = document.getElementById("hour");
 const minutes = document.getElementById("minutes");
 const ampm = document.getElementById("ampm");
+const stepsImage = document.getElementById("stepsImage");
+const stepsCount = document.getElementById("stepsCount");
 const hrtBeat = document.getElementById("hrtBeat");
 const hrtImage = document.getElementById("hrtImage");
 
@@ -20,10 +23,10 @@ clock.ontick = (evt) => {
   let today = evt.date;
   let hours = today.getHours();
   let mins = util.zeroPad(today.getMinutes());
-  
+
   if (preferences.clockDisplay === "12h") {
     // 12h format
-    ampm.text = hours >= 12 ? 'PM' : 'AM';
+    ampm.text = hours >= 12 ? "PM" : "AM";
     hours = util.zeroPad(hours % 12 ? hours : 12);
   } else {
     // 24h format
@@ -32,18 +35,22 @@ clock.ontick = (evt) => {
   // Update values
   hour.text = hours;
   minutes.text = mins;
-}
+};
 
 // Create a new instance of the HeartRateSensor object
 if (me.permissions.granted("access_heart_rate")) {
   let hrm = new HeartRateSensor();
-  
-  hrm.onreading = function() {
+
+  hrm.onreading = function () {
     // Peek the current sensor values
     hrtBeat.text = hrm.heartRate;
-    hrtImage.href = "like.png"
-  }
-  
+    hrtImage.href = "like.png";
+  };
+
   hrm.start();
-  
+}
+
+if (me.permissions.granted("access_activity")) {
+  stepsCount.text = today.adjusted.steps;
+  stepsImage.href = "footstep.png";
 }
